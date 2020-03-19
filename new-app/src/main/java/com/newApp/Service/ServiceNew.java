0b633b5a6e1a10c3795db17cb449e231852fdb1e
Rajat Dobriyal <Rajat Dobriyal@LAPTOP-LBR2V5I7>
2020-03-19 23:57:05 +0530

    One line change.

--- new-app/src/main/java/com/newApp/Service/ServiceNew.java
+++ new-app/src/main/java/com/newApp/Service/ServiceNew.java
@@ -3,6 +3,7 @@ package com.newApp.Service;
 public class ServiceNew {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.dir"));
 	}
 
 }
